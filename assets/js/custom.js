4cdbfb835de97aa0e6d678ec32638b8e8358d023
NguyenTheAnh <daubac403@gmail.com>
2023-09-25 11:43:21 +0900

    Add files via upload

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,6 @@
 const BIRTH_YEAR = 1991;
 const START_WORKING_YEAR = 2013;
+const END_DATE = "2081/01/01";
 
 const currentYear = new Date().getFullYear();
 
@@ -8,7 +9,14 @@ $(document).ready(function () {
     $(".data-age").text(currentYear - BIRTH_YEAR);
     $("#data-experienceYears").attr("data-purecounter-end", currentYear - START_WORKING_YEAR);
     $(".data-currentYear").text(currentYear);
+    $("#date-remainDays").attr("data-purecounter-end", dateDiffInDays(new Date(), new Date(END_DATE)));
 
     // Initiate Pure Counter js
     PureCounter();
 });
+
+// Calculate the number of days between 2 dates
+function dateDiffInDays(date1, date2) {
+    // round to the nearest whole number
+    return Math.round((date2 - date1) / (1000 * 60 * 60 * 24));
+}
