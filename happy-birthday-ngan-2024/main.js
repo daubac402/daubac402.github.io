7c1cf68798de4a78e6f25d7aa90a069b6c2e9736
NguyenTheAnh <daubac403@gmail.com>
2024-07-23 09:57:24 +0900

    Add files via upload

--- happy-birthday-ngan-2024/main.js
+++ happy-birthday-ngan-2024/main.js
@@ -4,8 +4,9 @@ Splitting();
 
 let particles = [];
 const colors = ["#eb6383","#fa9191","#ffe9c5","#b4f2e1"];
-function pop () {
-  for (let i = 0; i < 150; i++) {
+
+function pop() {
+  for (let i = 0; i < 100; i++) {
     const p = document.createElement('particule');
     p.x = window.innerWidth * 0.5;
     p.y = window.innerHeight + (Math.random() * window.innerHeight * 0.3);
@@ -19,19 +20,19 @@ function pop () {
     const size = Math.random() * 15 + 5;
     p.style.width = size + 'px';
     p.style.height = size + 'px';
-    p.style.background = colors[Math.floor(Math.random()*colors.length)];
+    p.style.background = colors[Math.floor(Math.random() * colors.length)];
     document.body.appendChild(p);
   }
 }
 
-function render () {
-  for (let i = particles.length - 1; i--; i > -1) {
+function render() {
+  for (let i = particles.length - 1; i >= 0; i--) {
     const p = particles[i];
     p.style.transform = `translate3d(${p.x}px, ${p.y}px, 1px)`;
-    
+
     p.x += p.vel.x;
     p.y += p.vel.y;
-    
+
     p.vel.y += (0.5 * p.mass);
     if (p.y > (window.innerHeight * 2)) {
       p.remove();
@@ -40,6 +41,10 @@ function render () {
   }
   requestAnimationFrame(render);
 }
+
 pop();
 window.setTimeout(render, 700);
+
+// Add event listeners for both click and touch events
 window.addEventListener('click', pop);
+window.addEventListener('touchstart', pop);
